568b44b80a19376842280ba67601426c4d720338
Panagiotis Karatakis <panagiotis_11@hotmail.com>
2019-09-27 11:27:53 +0300

    Add support for more attributes and complex queries

--- SAMLAffiliationPermit/SAMLAffiliationPermit.php
+++ SAMLAffiliationPermit/SAMLAffiliationPermit.php
@@ -21,16 +21,25 @@ class SAMLAffiliationPermit extends Limesurvey\PluginManager\PluginBase
     static protected $description = 'This plugin forces only SAML users with specific affiliation to participate on selected surveys';
     static protected $name = 'SAMLAffiliationPermit';
 
-    protected $settings = [
-        'affiliation_mapping' => [
-            'type' => 'string',
-            'label' => 'SAML Affiliation Attribute',
-            'default' => 'eduPersonPrimaryAffiliation'
-        ]
-    ];
+    protected $settings = [];
+    protected $surveySettings = [];
+    protected $filterFields = [];
 
     public function init()
     {
+        $this->addSurveySetting(
+            'person_filtering_enabled',
+            'checkbox',
+            'Enable Plugin',
+            'Enable users filtering based on SAML attributes',
+            false
+        );
+
+        $this->addField('affiliation', 'affiliations', 'eduPersonPrimaryAffiliation', 'faculty,student');
+        $this->addField('title', 'titles', 'title', 'whatever,Undergraduate Student');
+        $this->addField('department', 'departments', 'authDepartmentId', 'physics,hist');
+        $this->addField('status', 'statuses', 'urn:oid:1.3.6.1.4.1.7709.1.1.1.1.32', 'active|retired,whatever');
+
         $this->subscribe('beforeSurveySettings');
         $this->subscribe('newSurveySettings');
         $this->subscribe('beforeSurveyPage');
@@ -44,37 +53,11 @@ class SAMLAffiliationPermit extends Limesurvey\PluginManager\PluginBase
         if ($permission) {
             $event = $this->event;
 
+            $settings = $this->prepareSurveySettings($event);
+
             $event->set('surveysettings.' . $this->id, [
                 'name' => get_class($this),
-                'settings' => [
-                    'SAML_affiliation_permit_enabled' => [
-                        'type' => 'checkbox',
-                        'label' => 'Enabled',
-                        'help' => 'Enable the plugin for this survey',
-                        'default' => false,
-                        'current' => $this->get('SAML_affiliation_permit_enabled', 'Survey', $event->get('survey'), false),
-                    ],
-                    'allowed_affiliation' => [
-                        'type' => 'string',
-                        'label' => 'Allowed Affiliations',
-                        'help' => 'Comma separated, without spaces',
-                        'default' => 'faculty,student',
-                        'current' => $this->get('allowed_affiliation', 'Survey', $event->get('survey'), 'faculty,student'),
-                    ],
-                    'allowed_status_per_affiliation' => [
-                        'type' => 'string',
-                        'label' => 'Allowed Status per Affiliation',
-                        'help' => 'Comma separated, without spaces. One status rule per affiliation. Possible values (whatever, active, inactive, missing)',
-                        'default' => 'whatever|active,whatever|active',
-                        'current' => $this->get('allowed_status_per_affiliation', 'Survey', $event->get('survey'), 'active,whatever'),
-                    ],
-                    'affiliation_mapping_survey' => [
-                        'type' => 'string',
-                        'label' => 'SAML Affiliation Attribute',
-                        'default' => $this->get('affiliation_mapping', null, null, $this->settings['affiliation_mapping']['default']),
-                        'current' => $this->get('affiliation_mapping_survey', 'Survey', $event->get('survey'), $this->settings['affiliation_mapping']['default']),
-                    ]
-                ]
+                'settings' => $settings
             ]);
         }
     }
@@ -89,35 +72,14 @@ class SAMLAffiliationPermit extends Limesurvey\PluginManager\PluginBase
         }
     }
 
-    public function getAffiliation()
+    public function beforeSurveyPage()
     {
-        $AuthSAML = $this->pluginManager->loadPlugin('AuthSAML');
-
-        $ssp = $AuthSAML->get_saml_instance();
-
-        $ssp->requireAuth();
-
-        $attributes = $ssp->getAttributes();
-
-        $affiliationField = $this->get('affiliation_mapping_survey', 'Survey', $this->event->get('surveyId'));
-
-        return $attributes[$affiliationField][0];
+        $this->pluginGuard();
     }
 
-    public function getSurveyAllowedAffiliations()
+    public function afterSurveyComplete()
     {
-        $id = $this->getEvent()->get('surveyId');
-        $affiliations = $this->get('allowed_affiliation', 'Survey', $id);
-        $affiliations = explode(',', $affiliations);
-        return $affiliations;
-    }
-
-    public function getSurveyAllowedPersonStatuses()
-    {
-        $id = $this->getEvent()->get('surveyId');
-        $personStatus = $this->get('allowed_status_per_affiliation', 'Survey', $id);
-        $personStatus = explode(',', $personStatus);
-        return $personStatus;
+        $this->pluginGuard();
     }
 
     public function getGlobalBasePermissions() {
@@ -136,18 +98,51 @@ class SAMLAffiliationPermit extends Limesurvey\PluginManager\PluginBase
         ));
     }
 
-    public function beforeSurveyPage()
+    /*
+     Helper Functions
+    */
+
+    public function addField($field, $plural_field, $attribute, $default)
     {
-        $this->pluginGuard();
+        $this->filterFields[$plural_field] = $field;
+        $this->addGlobalSetting($field . '_mapping', 'string', 'SAML ' . ucfirst($field) . ' attribute', '', $attribute);
+        $this->addSurveySetting('allowed_' . $plural_field, 'string', 'Allowed ' . ucfirst($plural_field), 'Comma separated, without spaces.', $default);
     }
 
-    public function afterSurveyComplete()
+    public function addGlobalSetting($field, $type, $label, $help, $default)
     {
-        $this->pluginGuard();
+        $this->settings[$field] = [
+            'type' => $type,
+            'label' => $label,
+            'help' => $help,
+            'default' => $default
+        ];
+    }
+
+    public function addSurveySetting($field, $type, $label, $help, $default) {
+        $this->surveySettings[$field] = [
+            'type' => $type,
+            'label' => $label,
+            'help' => $help,
+            'default' => $default
+        ];
+    }
+
+    public function prepareSurveySettings($event)
+    {
+        return $this->array_map(function($key, $setting) use ($event) {
+            $setting['current'] = $this->get(
+                $key,
+                'Survey',
+                $event->get('survey'),
+                $this->surveySettings[$key]['default']
+            );
+            return $setting;
+        }, $this->surveySettings);
     }
 
     public function pluginGuard() {
-        $plugin_enabled = $this->get('SAML_affiliation_permit_enabled', 'Survey', $this->event->get('surveyId'));
+        $plugin_enabled = $this->get('person_filtering_enabled', 'Survey', $this->event->get('surveyId'), false);
 
         if ($plugin_enabled) {
             // If user is admin skip guard
@@ -159,29 +154,72 @@ class SAMLAffiliationPermit extends Limesurvey\PluginManager\PluginBase
                 }
             }
 
-            $person_affiliation = $this->getAffiliation();
+            $allowed_titles = $this->get('allowed_titles', 'Survey', $this->event->get('surveyId'), $this->surveySettings['allowed_titles']['default']);
+            $count = count(explode(',', $allowed_titles));
 
-            $affiliations = $this->getSurveyAllowedAffiliations();
-            if (!in_array($person_affiliation, $affiliations)) {
-                throw new CHttpException(403, gT("We are sorry but you are not allowed to participate in this survey."));
-            }
-
-            $PersonStatus = $this->pluginManager->loadPlugin('SAMLPersonStatus');
-            if ($PersonStatus !== null) {
-                $person_status = $PersonStatus->getPersonStatus();
-
-                $person_statuses = $this->getSurveyAllowedPersonStatuses();
-
-                if (count($person_statuses) !== count($affiliations)) {
-                    throw new CHttpException(500, gT("Affiliation count does not match Status per Affiliation count."));
+            for ($index = 0; $index < $count; $index++) {
+                $flag = false;
+                foreach ($this->filterFields as $plural_field => $field) {
+                    $flag = $this->checkField($field, $plural_field, $index);
+                    if (!$flag) {
+                        continue 2;
+                    }
                 }
 
-                array_map(function ($affiliation, $status) use ($PersonStatus, $person_affiliation, $person_status) {
-                    if ($affiliation === $person_affiliation && !$PersonStatus->checkPersonStatus($person_status, $status)) {
-                        throw new CHttpException(403, gT("We are sorry but you do not meet the required affiliation person status to participate in this survey."));
-                    }
-                }, $affiliations, $person_statuses);
+                if ($flag) {
+                    return;
+                }
             }
+            throw new CHttpException(403, gT("We are sorry but you do not meet the required affiliation person status to participate in this survey."));
         }
+    }
+
+    public function getSAMLValue($field)
+    {
+        $AuthSAML = $this->pluginManager->loadPlugin('AuthSAML');
+
+        $ssp = $AuthSAML->get_saml_instance();
+
+        $ssp->requireAuth();
+
+        $attributes = $ssp->getAttributes();
+
+        $attribute = $this->get($field . '_mapping', null, null, $this->settings[$field . '_mapping']['default']);
+
+        if (isset($attributes[$attribute][0])) {
+            return $attributes[$attribute][0];
+        }
+        return 'missing';
+    }
+
+    public function checkField($field, $field_plural, $index)
+    {
+        $rules = $this->get('allowed_' . $field_plural, 'Survey', $this->event->get('surveyId'), $this->surveySettings['allowed_' . $field_plural]['default']);
+        $rules = explode(',', $rules);
+
+        if (!isset($rules[$index])) {
+            throw new CHttpException(500, gT('Rules count does not match.'));
+        }
+
+        $value = $this->getSAMLValue($field);
+
+        return $this->checkRules($value, $rules[$index]);
+    }
+
+    public function checkRules($value, $rules)
+    {
+        $rules = explode('|', $rules);
+
+        if (in_array('whatever', $rules) || in_array($value, $rules)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private function array_map($fn, $array) {
+        $keys = array_keys($array);
+        $valuesMapped = array_map($fn, $keys, $array);
+        return array_combine($keys, $valuesMapped);
     }
 }
